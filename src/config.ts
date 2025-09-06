fba7c273b7bde3e39daa7cbcf5cad602ffd18dec
Abhishek Singh <87878794+3x10RaiseTo8@users.noreply.github.com>
2025-09-06 12:58:29 +0530

    Update website url

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ export const SITE = {
   title: "OpenSourceDay 2025",
   description:
     "OpenSourceDay is all about giving back to the open source projects, sharpening your skills, and celebrating all things open source. Join us for a day!",
-  website: "https://osweekend.org",
+  website: "https://opensourceweekend.org",
   ogImage: "/images/OSWBlack.png",
   icon: "/icons/OSDLogo.svg",
 };
