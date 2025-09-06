8c4fa6851057a04fa0db2e72e9ed6feaec4cdc32
Abhishek Singh <87878794+3x10RaiseTo8@users.noreply.github.com>
2025-09-06 12:43:24 +0530

    add tailwindcss extension in recommendation

---
+++ src/config.ts
@@ -0,0 +1,44 @@
+// import { House, Library, Ticket, type Icon as IconType } from "@lucide/astro";
+
+export const SITE = {
+  title: "OpenSourceDay 2025",
+  description:
+    "OpenSourceDay is all about giving back to the open source projects, sharpening your skills, and celebrating all things open source. Join us for a day!",
+  website: "https://osweekend.org",
+  ogImage: "/images/OSWBlack.png",
+  icon: "/icons/OSDLogo.svg",
+};
+
+// type Item = {
+//   name: string;
+//   href: string;
+//   icon: typeof IconType | string;
+//   linkText?: string;
+// };
+
+// const socials: Item[] = [
+//   {
+//     name: "LinkedIn",
+//     href: "",
+//     icon: "aaaaa",
+//     linkText: "Follow us on LinkedIn",
+//   },
+// ];
+
+// const menuItems: Item[] = [
+//   {
+//     name: "Home",
+//     href: "/",
+//     icon: House,
+//   },
+//   {
+//     name: "Blog",
+//     href: "/blog",
+//     icon: Library,
+//   },
+//   {
+//     name: "Tickets",
+//     href: "",
+//     icon: Ticket,
+//   },
+// ];
