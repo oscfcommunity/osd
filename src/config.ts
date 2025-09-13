2cd932a661b47809935903bbeae80964b08fab36
Ashish Zapadiya <ashishzapadiya834@gmail.com>
2025-09-13 12:13:45 +0530

    feat: add google calendar in schedule page

--- src/config.ts
+++ src/config.ts
@@ -95,6 +95,13 @@ export const SITE = {
   icon: "@/assets/icons/OSDGreen.svg",
 };
 
+export const fullEvent = {
+  title: "Open Source Day 2025",
+  room: "VENUE - TBA Ahmedabad",
+  start: "20251005T080000Z",
+  end: "20251005T173000Z",
+};
+
 export const eventSchedule = [
   {
     time: "08:00 AM - 09:30 AM",
@@ -102,6 +109,8 @@ export const eventSchedule = [
     speaker: "",
     type: "registration",
     room: "Lobby/Food Court",
+    start: "20251005T080000Z",
+    end: "20251005T093000Z",
   },
   {
     time: "09:30 AM - 10:00 AM",
@@ -109,6 +118,8 @@ export const eventSchedule = [
     speaker: "",
     type: "activity",
     room: "Main Auditorium",
+    start: "20251005T093000Z",
+    end: "20251005T100000Z",
   },
   {
     time: "10:00 AM - 10:15 AM",
@@ -116,6 +127,8 @@ export const eventSchedule = [
     speaker: "",
     type: "opening",
     room: "Main Auditorium",
+    start: "20251005T100000Z",
+    end: "20251005T101500Z",
   },
   {
     time: "10:15 AM - 10:45 AM",
@@ -123,6 +136,8 @@ export const eventSchedule = [
     speaker: "",
     type: "keynote",
     room: "Main Auditorium",
+    start: "20251005T101500Z",
+    end: "20251005T104500Z",
   },
   {
     time: "10:45 AM - 11:30 AM",
@@ -130,6 +145,8 @@ export const eventSchedule = [
     speaker: "",
     type: "talk",
     room: "Main Auditorium",
+    start: "20251005T104500Z",
+    end: "20251005T113000Z",
   },
   {
     time: "11:30 AM - 12:15 PM",
@@ -137,6 +154,8 @@ export const eventSchedule = [
     speaker: "",
     type: "talk",
     room: "Main Auditorium",
+    start: "20251005T113000Z",
+    end: "20251005T121500Z",
   },
   {
     time: "12:15 PM - 01:00 PM",
@@ -144,6 +163,8 @@ export const eventSchedule = [
     speaker: "",
     type: "panel",
     room: "Main Auditorium",
+    start: "20251005T121500Z",
+    end: "20251005T130000Z",
   },
   {
     time: "01:00 PM - 02:00 PM",
@@ -151,6 +172,8 @@ export const eventSchedule = [
     speaker: "",
     type: "break",
     room: "Food Court",
+    start: "20251005T130000Z",
+    end: "20251005T140000Z",
   },
   {
     time: "02:00 PM - 02:45 PM",
@@ -158,6 +181,8 @@ export const eventSchedule = [
     speaker: "",
     type: "talk",
     room: "Main Auditorium",
+    start: "20251005T140000Z",
+    end: "20251005T144500Z",
   },
   {
     time: "02:45 PM - 03:30 PM",
@@ -165,6 +190,8 @@ export const eventSchedule = [
     speaker: "",
     type: "talk",
     room: "Main Auditorium",
+    start: "20251005T144500Z",
+    end: "20251005T153000Z",
   },
   {
     time: "03:30 PM - 04:15 PM",
@@ -172,6 +199,8 @@ export const eventSchedule = [
     speaker: "",
     type: "panel",
     room: "Main Auditorium",
+    start: "20251005T153000Z",
+    end: "20251005T161500Z",
   },
   {
     time: "04:15 PM - 04:45 PM",
@@ -179,6 +208,8 @@ export const eventSchedule = [
     speaker: "",
     type: "keynote",
     room: "Main Auditorium",
+    start: "20251005T161500Z",
+    end: "20251005T164500Z",
   },
   {
     time: "04:45 PM - 05:00 PM",
@@ -186,6 +217,8 @@ export const eventSchedule = [
     speaker: "",
     type: "closing",
     room: "Main Auditorium",
+    start: "20251005T164500Z",
+    end: "20251005T170000Z",
   },
   {
     time: "05:00 PM - 05:30 PM",
@@ -193,6 +226,8 @@ export const eventSchedule = [
     speaker: "",
     type: "social",
     room: "Exhibition Hall",
+    start: "20251005T170000Z",
+    end: "20251005T173000Z",
   },
 ];
 
