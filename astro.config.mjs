51cf56f3347f1639485e71a4fcb98a2f0e171a5d
Abhishek Singh <87878794+3x10RaiseTo8@users.noreply.github.com>
2026-02-22 11:43:39 +0530

    Add form redirects for volunteering, partners, speakers, sponsors & evangelists

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,29 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 import partytown from "@astrojs/partytown";
 
+const REDIRECTS = {
+  // call for volunteers
+  "/volunteer": "https://forms.gle/TbPEKUg2eet3VWuF7",
+  "/cfv": "/volunteer",
+  // call for community partners
+  "/cfcp": "https://forms.gle/pkZUerJzfvYYGJLs5",
+  "/cfc": "/cfcp",
+  "/cfp": "/cfcp",
+  // call for speakers
+  "/speak": "https://forms.gle/9NWA4PupC558Ery49",
+  "/cfspk": "/speak",
+  //call for sponsors
+  "/sponsor": "https://forms.gle/5LkeXLPKezt3u3G78",
+  "/cfs": "/sponsor",
+  // call for evangelists
+  "/evangelize": "https://forms.gle/3RCMnSiKfbq58o246",
+  "/cfe": "/evangelize",
+};
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://osd.opensourceweekend.org",
+  redirects: REDIRECTS,
   vite: {
     // @ts-ignore
     plugins: [tailwindcss()],
