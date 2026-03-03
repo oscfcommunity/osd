4f9829118a710261ac98823b64328283eadcae80
Ashish Zapadiya <ashishzapadiya834@gmail.com>
2026-03-04 02:46:34 +0530

    feat: Update Prettier configuration and enhance env variable defaults for Strapi integration

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,5 +1,5 @@
 export const env = {
-  STRAPI_BASE_URL: import.meta.env.STRAPI_BASE_URL,
-  STRAPI_API_TOKEN: import.meta.env.STRAPI_API_TOKEN,
-  STRAPI_GRAPHQL_URL: import.meta.env.STRAPI_GRAPHQL_URL,
+  STRAPI_BASE_URL: import.meta.env.STRAPI_BASE_URL || "https://cms.opensourceweekend.org",
+  STRAPI_GRAPHQL_URL: import.meta.env.STRAPI_GRAPHQL_URL || "https://cms.opensourceweekend.org/graphql",
+  STRAPI_API_TOKEN: import.meta.env.STRAPI_API_TOKEN || "",
 };
